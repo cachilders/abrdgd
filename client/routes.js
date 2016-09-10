b54f6837d81bfbd90b2a014a39103b6ac160b1fa
Casey Childers <cachilders@gmail.com>
2016-09-10 08:49:29 -0700

    Update ui-router config

--- client/routes.js
+++ client/routes.js
@@ -2,19 +2,27 @@ var abrdgd = angular.module('abrdgd', ['ui.router']);
 
 abrdgd.config(function($stateProvider) { // Angular requires this config block to register injected provider within block
 
-  var aState = {
-    name: 'first',
-    url: '/just-a-long-url',
-    template: '<h3>I\'m the first state!</h3>'
+  var home = {
+    name: 'home',
+    url: '/',
+    views: {
+      '': {
+        template: '<h3>I\'m the home view!</h3>'
+      }
+    }
   }
 
-  var bState = {
-    name: 'second',
-    url: '/shorty',
-    template: '<h3>I\'m the other guy!</h3>'
+  var about = {
+    name: 'about',
+    url: '/about',
+    views: {
+      'about': {
+        template:'<h3>I\'m the about view!</h3>'
+      }
+    }
   }
 
-  $stateProvider.state(aState);
-  $stateProvider.state(bState);
+  $stateProvider.state(home);
+  $stateProvider.state(about);
 
 });
