3b9023c5828a55d90c63986dfdf01ffa07dd6933
Casey Childers <cachilders@gmail.com>
2016-09-11 22:47:52 -0700

    [feature] Implement server side user auth framework

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
-const express     = require('express');
-const partials    = require('express-partials');
-const session     = require('express-session');
-const bodyParser  = require('body-parser');
-const fs          = require('fs');
-const db          = require('./db-config');
+const express       = require('express');
+const partials      = require('express-partials');
+const session       = require('express-session');
+const bodyParser    = require('body-parser');
+const fs            = require('fs');
+const passport      = require('passport');
+const cookieParser  = require('cookie-parser');
+const strategy      = require('./server/setup-passport');
+const db            = require('./server/db-config');
 
 const app = express();
 
@@ -14,9 +17,34 @@ app.listen(port);
 
 app.use(express.static(__dirname + "/client"));
 
+app.use(cookieParser());
+app.use(session({ secret: process.env.AUTH0_CLIENT_SECRET, resave: false,  saveUninitialized: false }));
+
+// Routes
+
 app.get('/', (req, res) => res.redirect('/index.html'))  // Ultimately needs to be /public
+
 app.get('/furthermore', (req, res) => res.send('Sup.'))
 
+// Auth Routes (TODO: move the meat of these to their own module)
+app.get('/authCallback',
+  passport.authenticate('auth0', { failureRedirect: '/url-if-something-fails' }),
+  function(req, res) {
+    if (!req.user) {
+      throw new Error('user null');
+    }
+    res.redirect("/user");
+  });
+
+app.get('/user', function (req, res) {
+  res.render('user', {
+    user: req.user
+  });
+});
+
 console.log('Server now listening on port ' + port);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 module.exports = app;
