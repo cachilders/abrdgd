f1f60ea8519f0fdb2db5258c46a3c78ac48a7c7f
Casey Childers <cachilders@gmail.com>
2016-09-11 23:39:44 -0700

    [bug out] Comment out all auth services in server

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.listen(port);
 
 app.use(express.static(__dirname + "/client"));
 
-app.use(cookieParser());
-app.use(session({ secret: process.env.AUTH0_CLIENT_SECRET, resave: false,  saveUninitialized: false }));
+// app.use(cookieParser()); // Uncomment for authentication services
+// app.use(session({ secret: process.env.AUTH0_CLIENT_SECRET, resave: false,  saveUninitialized: false }));
 
 // Routes
 
@@ -28,24 +28,25 @@ app.get('/', (req, res) => res.redirect('/index.html'))  // Ultimately needs to 
 app.get('/furthermore', (req, res) => res.send('Sup.'))
 
 // Auth Routes (TODO: move the meat of these to their own module)
-app.get('/authCallback',
-  passport.authenticate('auth0', { failureRedirect: '/url-if-something-fails' }),
-  function(req, res) {
-    if (!req.user) {
-      throw new Error('user null');
-    }
-    res.redirect("/user");
-  });
 
-app.get('/user', function (req, res) {
-  res.render('user', {
-    user: req.user
-  });
-});
+// app.get('/authCallback', // Uncomment for authentication services
+//   passport.authenticate('auth0', { failureRedirect: '/url-if-something-fails' }),
+//   function(req, res) {
+//     if (!req.user) {
+//       throw new Error('user null');
+//     }
+//     res.redirect("/user");
+//   });
+
+// app.get('/user', function (req, res) {
+//   res.render('user', {
+//     user: req.user
+//   });
+// });
 
 console.log('Server now listening on port ' + port);
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize()); // Uncomment for authentication services
+// app.use(passport.session());
 
 module.exports = app;
